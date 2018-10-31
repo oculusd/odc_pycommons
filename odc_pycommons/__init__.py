e6ccbd51010fdea7bf6b9f0bd41d13627f9dcffc
Nico Coetzee <nicc777@gmail.com>
2018-10-31 06:52:01 +0200

    Added HOME constant

---
+++ odc_pycommons/__init__.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2018. All rights reserved. OculusD.com, Inc. Please refer to the LICENSE.txt file for full license information. Licensed in terms of the GPLv3 License.
+
+import pathlib
+import os
+
+HOME = '{}{}.oculusdcli'.format(
+    str(pathlib.Path.home()),
+    os.sep
+)
+
+
+# EOF
