28ed820aed968c4678978e8792025ae6ba03f5fe
Nico Coetzee <info@oculusd.com>
2019-06-07 06:53:24 +0200

    Issue #14 - Started with the models

--- odc_pycommons/models.py
+++ odc_pycommons/models.py
@@ -141,4 +141,64 @@ AXIS_DATA_TYPES = {
 }
 
 
+def _template_field_to_json(
+    input_field_name: str,
+    input_field_value: object=None,
+    can_be_none: bool=True,
+    is_required: bool=False
+)->str:
+    return_value = ''
+    if input_field_value is not None:
+        if isinstance(input_field_value, ApiJsonBodyElement):
+            return_value = json.dumps({input_field_name: input_field_value.default_to_json()})
+        else:
+            return_value = json.dumps({input_field_name: input_field_value})
+    elif can_be_none is False and is_required is True:
+        raise Exception('value cannot be none for field named "{}"'.format(input_field_name))
+    elif can_be_none is True and is_required is True:
+        return_value = json.dumps({input_field_name: None})
+    else: 
+        return_value = json.dumps({input_field_name: None})
+    return return_value
+
+
+class ApiJsonBodyElement:
+
+    def __init__(
+        self,
+        field_name: str,
+        can_be_none: bool=True,
+        default_value: object=None,
+        start_value: object=None,
+        to_json_function: object=None,
+        is_required: bool=False
+    ):
+        self.field_name = field_name
+        self.can_be_none = can_be_none
+        self.default_value = default_value
+        self.value = start_value
+        self.to_json_function = _template_field_to_json
+        if to_json_function is not None:
+            if callable(to_json_function):
+                self.to_json_function = to_json_function
+        self.is_required = is_required
+
+    def set_value(self, value: object=None):
+        self.value = value
+
+    def to_json(self):
+        return self.to_json_function(
+            input_field_name=self.field_name,
+            input_field_value=self.value,
+            can_be_none=self.can_be_none,
+            is_required=self.is_required
+        )
+
+
+class ApiJsonBody:
+
+    def __init__(self, body_template: str):
+        pass
+
+
 # EOF
