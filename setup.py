538891a328263559b863b72a0269f86beceb89a6
Nico Coetzee <info@oculusd.com>
2019-06-13 07:09:12 +0200

    Cleanup and update for PyPI release

--- setup.py
+++ setup.py
@@ -8,15 +8,15 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='odc_pycommons',  # Required
-    version='0.3.1',  # Required
-    description='OculusD Python Commons Library',  # Optional
-    long_description=long_description,  # Optional
-    long_description_content_type='text/markdown',  # Optional (see note above)
-    url='https://www.oculusd.com/',  # Optional
-    author='OculusD',  # Optional
-    author_email='info@oculusd.com',  # Optional
-    classifiers=[  # Optional
+    name='odc_pycommons',
+    version='0.3.1',
+    description='OculusD Python Commons Library',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://www.oculusd.com/',
+    author='OculusD',
+    author_email='info@oculusd.com',
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Topic :: Internet',
@@ -25,17 +25,18 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
-    keywords='cli library iot oculusd',  # Optional
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
-    install_requires=['pyyaml'],  # Optional
-    extras_require={  # Optional
+    keywords='cli library iot oculusd',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=['pyyaml'],
+    extras_require={
         'dev': ['check-manifest'],
         'test': ['coverage'],
     },
-    project_urls={  # Optional
+    project_urls={
         'Bug Reports': 'https://www.oculusd.com/',
         'Funding': 'https://www.oculusd.com/',
         'Say Thanks!': 'https://www.oculusd.com/',
         'Source': 'https://www.oculusd.com/',
     },
+    download_url='https://github.com/oculusd/odc_pycommons/archive/release-0.3.1.tar.gz',
 )
